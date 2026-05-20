b4caaa107fad5e0a0a201aa736355fbb7a9584ac
lopes <lopesadilson948@gmail.com>
2026-05-21 00:20:38 +0100

    refactor: update jsonwebtoken import structure and pin package version

--- api/_lib/auth.ts
+++ api/_lib/auth.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import * as jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 
 const supabase = createClient(
   process.env.VITE_SUPABASE_URL!,
